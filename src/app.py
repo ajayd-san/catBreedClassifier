d1e51f8406b0b61f73b0bdbeb677968708f79ae9
default-303 <ajayds2001@gmail.com>
2022-03-03 11:01:11 +0530

    added info in footer

--- src/app.py
+++ src/app.py
@@ -1,13 +1,27 @@
 import streamlit as st
 from predictor import predict
 
+footer = """
+<style>
+
+footer: {
+    visibility: visible;
+}
+footer:after{
+    content: ' by Ajay DS';
+}
+</style>
+"""
+
+
+
 st.title('Cat Breed Classifier')
 
 upload_image = st.file_uploader('Upload cat image', type=['jpg', 'jpeg', 'png'])
-
+st.markdown(footer, unsafe_allow_html=True)
 if upload_image is not None:
     predictions = predict(upload_image)
 
-    st.write('## Meow!! Here are the predictions:')
+    st.write('## Meow!! Here are the top 3 predictions:')
     for prediction in predictions: 
         st.write(f"{prediction['label']}: {prediction['proba']:.2f}%")
