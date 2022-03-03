2f1164457ab82eb782ab0662796854ec9ffa1c48
default-303 <ajayds2001@gmail.com>
2022-03-03 11:46:43 +0530

    added warning component on trained breeds

--- src/app.py
+++ src/app.py
@@ -12,16 +12,27 @@ footer:after{
 }
 </style>
 """
+st.markdown(footer, unsafe_allow_html=True)
 
 
 
 st.title('Cat Breed Classifier')
 
-upload_image = st.file_uploader('Upload cat image', type=['jpg', 'jpeg', 'png'])
-st.markdown(footer, unsafe_allow_html=True)
+upload_image = st.file_uploader("Upload your cat's image:", type=['jpg', 'jpeg', 'png'])
 if upload_image is not None:
     predictions = predict(upload_image)
 
     st.write('## Meow!! Here are the top 3 predictions:')
     for prediction in predictions: 
-        st.write(f"{prediction['label']}: {prediction['proba']:.2f}%")
+        st.write(f"{prediction['label']} - {prediction['proba']:.2f}%")
+
+    st.warning(
+        """
+        The classifier has been trained only on 12 common breeds (listed below), wrong prediction or weak confidence\
+        in a prediction is maybe due the model not being trained on that specific breed.
+
+        The breeds trained are: Abyssinian, Bengal, Birman, Bombay, British Shorthair,
+       Egyptian Mau, Maine Coon, Persian, Ragdoll, Russian Blue,
+       Siamese, and Sphynx.
+        """
+    )
